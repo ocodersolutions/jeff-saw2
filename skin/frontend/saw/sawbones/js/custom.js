556bd57cd0ac8ec174e8c89f6d654e48f3d5e508
Quang Tran <topinfo93@gmail.com>
2016-10-25 16:33:33 +0700

    update login header

--- skin/frontend/saw/sawbones/js/custom.js
+++ skin/frontend/saw/sawbones/js/custom.js
@@ -3,5 +3,13 @@ document.observe("dom:loaded", function() {
   $j("span.fa-blue").click(function(){
   	$j(this).closest('li').parent()
   	$j(this).closest('li').toggleClass('open-sub');
-  })//depends on a div id.
+  });
+	$j('#btn-account ,#header-account').hover(
+  		function() {
+  			$j('#header-account').css('display','block');
+  		}, function() {
+    		$j('#header-account').css('display','none');
+  		}
+	);
 });
+
